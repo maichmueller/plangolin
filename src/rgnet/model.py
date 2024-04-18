c0e67dfa9936bddd8c12910ff4ba27e29038329a
jkrude <jakob.krude@hotmail.com>
2024-04-18 17:30:47 +0200

    Introduce lightning

--- src/rgnet/model.py
+++ src/rgnet/model.py
@@ -1,23 +1,34 @@
+import lightning as L
+import torch
+import torch.nn.functional as F
 import torch_geometric as pyg
-from torch import nn
+from torch import nn, Tensor
+from torch.nn import LayerNorm, ReLU
+from torch_geometric.nn import DeepGCNLayer, GENConv
 
 
-class PureGNN(nn.Module):
+class PureGNN(L.LightningModule):
 
     def __init__(self, in_channel: int, embedding_size: int, num_layer: int) -> None:
         super().__init__()
         self.linear = nn.Linear(in_channel, embedding_size)
-        self.layer = nn.ModuleList(
-            [
-                pyg.nn.GENConv(
-                    in_channels=embedding_size,
-                    out_channels=embedding_size,
-                    aggr="softmax",
-                    node_dim=0,
-                )
-                for _ in range(num_layer)
-            ]
-        )
+        self.layer = nn.ModuleList()
+        for i in range(num_layer):
+            conv = GENConv(
+                embedding_size,
+                embedding_size,
+                aggr="softmax",
+                learn_t=False,
+                num_layers=2,
+                norm="layer",
+                node_dim=0,
+            )
+            norm = LayerNorm(embedding_size, elementwise_affine=True)
+            act = ReLU(inplace=True)
+
+            layer = DeepGCNLayer(conv, norm, act, block="res+", dropout=0.1)
+            self.layer.append(layer)
+
         self.readout = nn.Sequential(
             nn.Linear(embedding_size, embedding_size),
             nn.Tanh(),
@@ -32,13 +43,40 @@ class PureGNN(nn.Module):
         :return: Value estimate for every input graph, tensor of shape [32, 1]
         """
         x = self.linear(x.view(-1, 1))
-        for gcn_layer in self.layer:
-            x = gcn_layer(x, edge_index)
-            x.relu()
+        x = self.layer[0].conv(x, edge_index)
+        for layer in self.layer[1:]:
+            x = layer(x, edge_index)
+
+        x = self.layer[0].act(self.layer[0].norm(x))
         # sum-up the embeddings for each graph-node -> shape [embedding_size,batch_size]
         aggregated = pyg.nn.global_add_pool(x, batch)
         # reduce from embeddings_size to one -> shape [batch_size, 1]
-        return self.readout(aggregated)
+        out = self.readout(aggregated)
+        return out.view(-1)  # shape [batch_size]
+
+    def training_step(self, batch, batch_index) -> torch.Tensor:
+        x, edge_index = batch.x, batch.edge_index
+        out = self(x, edge_index, batch.batch)
+        loss: Tensor = F.l1_loss(out, batch.y)
+        self.log("train_loss", loss, batch_size=batch.batch_size)
+        return loss
+
+    def configure_optimizers(self) -> torch.optim.Optimizer:
+        return torch.optim.Adam(self.parameters(), lr=0.01, weight_decay=5e-4)
+
+    def _val_test_step(self, batch, phase: str):
+        x, edge_index = batch.x, batch.edge_index
+        x_out = self.forward(x, edge_index, batch.batch)
+
+        loss = F.l1_loss(x_out, batch.y)
+        self.log(f"{phase}_loss", loss, batch_size=batch.batch_size)
+        return x_out, loss, batch.y
+
+    def validation_step(self, batch, batch_index):
+        return self._val_test_step(batch, "val")
+
+    def test_step(self, batch, batch_index):
+        return self._val_test_step(batch, "test")
 
     def num_parameter(self) -> int:
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
--- src/rgnet/supervised/run.py
+++ src/rgnet/supervised/run.py
@@ -5,65 +5,108 @@ import time
 from datetime import datetime
 
 import torch
+import torch_geometric as pyg
+import wandb
+from lightning.pytorch import seed_everything, Trainer
+from lightning.pytorch.loggers import WandbLogger
 
 from rgnet.encoding import ColorGraphEncoder
+from rgnet.model import PureGNN
 from rgnet.pddl_import import import_all_from, import_problems
 from rgnet.supervised.data import MultiInstanceSupervisedSet
-from rgnet.supervised.training import Trainer
 from rgnet.utils import get_device_cuda_if_possible, time_delta_now
 
 
-def run():
+def _dataset_of(problems, domain, root):
+    return MultiInstanceSupervisedSet(
+        problems, ColorGraphEncoder(domain), root=root, log=True
+    )
+
+
+def _setup_datasets(problem_path: str, dataset_path: str, batch_size: int):
+    domain, problems = import_all_from(problem_path)
+    training_set = _dataset_of(problems, domain, dataset_path + "/train")
+    train_loader = pyg.loader.DataLoader(training_set, batch_size, shuffle=True)
+
+    evaluation_set = _dataset_of(
+        import_problems(problem_path + "/eval", domain), domain, dataset_path + "/eval"
+    )
+    # Test the model
+    test_set = _dataset_of(
+        import_problems(problem_path + "/test", domain), domain, dataset_path + "/test"
+    )
+    eval_loader, test_loader = [
+        pyg.loader.DataLoader(dset, batch_size, shuffle=False)
+        for dset in (evaluation_set, test_set)
+    ]
+
+    logging.info(f"Training dataset contains {len(training_set)} graphs/states")
+    logging.info(f"Evaluation dataset contains {len(evaluation_set)} graphs/states")
+
+    return train_loader, eval_loader, test_loader
+
+
+def run(
+    epochs=1000,
+    embedding_size=32,
+    num_layer=24,
+    batch_size=64,
+):
     curr_dir = os.getcwd()
     logging.getLogger().setLevel(logging.INFO)
     device: torch.device = get_device_cuda_if_possible()
     logging.info(f"Using {device.type} as device")
     logging.info("Working from " + curr_dir)
     start_time = time.time()
+    time_stamp = datetime.now().strftime("%y%m%d_%H%M%S")
+    wlogger = WandbLogger(project="rgnet", name="supervised_blocks" + time_stamp)
+    wlogger.experiment.config.update(
+        {
+            "epoch": epochs,
+            "embedding_size": embedding_size,
+            "num_layer": num_layer,
+            "device": device.type,
+        },
+    )
+
+    seed_everything(42, workers=True)
 
     # define paths
     data_path = curr_dir + "/data"
     problem_path = data_path + "/pddl_domains/blocks"
-    test_problem_path = problem_path + "/test"
     dataset_path = data_path + "/datasets/blocks"
-    training_set_path = dataset_path + "/training"
-    test_set_path = dataset_path + "/test"
-    model_save_path = pathlib.Path(
-        data_path + f"/models/run{datetime.now().strftime('%y%m%d_%H%M%S')}.pt"
-    )
+    model_save_path = pathlib.Path(data_path + f"/models/run{time_stamp}.pt")
     model_save_path.parent.mkdir(exist_ok=True)
 
     import_time = time.time()
-    domain, problems = import_all_from(problem_path)
-    training_set = MultiInstanceSupervisedSet(
-        problems, ColorGraphEncoder(domain), root=training_set_path, log=True
-    )
-    # Evaluate the model
-    evaluation_problems = import_problems(test_problem_path, domain)
-    evaluation_set = MultiInstanceSupervisedSet(
-        evaluation_problems, ColorGraphEncoder(domain), root=test_set_path
-    )
-    logging.info(f"Took {time_delta_now(import_time)} to construct the dataset.")
 
-    logging.info(f"Training dataset contains {len(training_set)} graphs/states")
-    # logging.info(training_set.get_summary())
+    train_loader, eval_loader, test_loader = _setup_datasets(
+        problem_path, dataset_path, batch_size
+    )
+
+    logging.info(f"Took {time_delta_now(import_time)} to construct the datasets.")
+
     start_time_training = time.time()
-    logging.info("Starting training")
-    trainer = Trainer(
-        train_set=training_set,
-        test_set=evaluation_set,
-        epochs=100,
-        embedding_size=16,
-        num_layer=24,
-        save_file=model_save_path,
-    )
-    trainer.train()
 
+    model = PureGNN(in_channel=1, embedding_size=embedding_size, num_layer=num_layer)
+    wlogger.watch(model)
+    wlogger.experiment.config.update({"num_parameter": model.num_parameter()})
+
+    trainer = Trainer(accelerator="gpu", devices=1, max_epochs=epochs, logger=wlogger)
+
+    logging.info("Starting training")
+    trainer.fit(
+        model=model, train_dataloaders=train_loader, val_dataloaders=eval_loader
+    )
     logging.info(f"Took {time_delta_now(start_time_training)} to train the model")
 
     logging.info(f"Saved model can be found at {model_save_path}")
 
+    trainer.test(model, dataloaders=test_loader)
+
     logging.info(f"Completed run after {time_delta_now(start_time)}.")
+
+    wandb.finish(quiet=True)
 
 
 if __name__ == "__main__":
