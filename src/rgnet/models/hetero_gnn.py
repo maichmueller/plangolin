c884296466a19d2f95252fcbb6475c3ed7c4409c
jkrude <jakob.krude@hotmail.com>
2024-05-07 17:01:52 +0200

    Remove normalization for comparability with supervised paper && logged loss by label

--- src/rgnet/models/hetero_gnn.py
+++ src/rgnet/models/hetero_gnn.py
@@ -1,10 +1,12 @@
-from typing import Dict, Optional
+from collections import defaultdict
+from typing import Dict, List, Optional
 
 import torch
-import torch.nn.functional as F
 import torch_geometric as pyg
+import wandb
 from lightning import LightningModule
 from torch import Tensor
+from torch.nn.modules.loss import L1Loss, MSELoss, _Loss
 from torch_geometric.typing import Adj
 
 from rgnet.models.hetero_message_passing import FanInMP, FanOutMP
@@ -35,13 +37,8 @@ class HeteroGNN(torch.nn.Module):
             # One MLP per predicate (goal-predicates included)
             # For a predicate p(o1,...,ok) the corresponding MLP gets k object
             # embeddings as input and generates k outputs, one for each object.
-            pred: pyg.nn.MLP(
-                [
-                    arity * hidden_size,
-                    hidden_size,
-                    arity * hidden_size,
-                ],
-                norm="layer_norm",  # necessary for batches of size 1
+            pred: HeteroGNN.mlp(
+                hidden_size * arity, hidden_size * arity, hidden_size * arity
             )
             for pred, arity in arity_dict.items()
             if arity > 0
@@ -49,11 +46,12 @@ class HeteroGNN(torch.nn.Module):
 
         self.obj_to_atom = FanOutMP(mlp_dict, src_name=obj_type_id)
 
-        self.obj_update = pyg.nn.MLP([hidden_size * 2, hidden_size * 2, hidden_size])
-        self.atom_to_obj = FanInMP(hidden_size=hidden_size, dst_name=obj_type_id)
-        self.readout = pyg.nn.MLP(
-            [hidden_size, 2 * hidden_size, 1], act="Mish", norm="layer_norm"
+        self.obj_update = HeteroGNN.mlp(
+            in_size=2 * hidden_size, hidden_size=2 * hidden_size, out_size=hidden_size
         )
+
+        self.atom_to_obj = FanInMP(hidden_size=hidden_size, dst_name=obj_type_id)
+        self.readout = HeteroGNN.mlp(hidden_size, 2 * hidden_size, 1)
 
     def encoding_layer(self, x_dict: Dict[str, Tensor]):
         # Resize everything by the hidden_size
@@ -104,37 +102,77 @@ class HeteroGNN(torch.nn.Module):
         # Produce final single scalar of shape [1]
         return self.readout(aggr).view(-1)
 
+    @staticmethod
+    def mlp(in_size: int, hidden_size: int, out_size: int):
+        return pyg.nn.MLP([in_size, hidden_size, out_size], norm=None, dropout=0.0)
+
 
 class LightningHetero(LightningModule):
-    def __init__(self, lr: float = 0.001, weight_decay: float = 5e-4, **kwargs) -> None:
+    def __init__(
+        self,
+        lr: float = 0.001,
+        weight_decay: float = 5e-4,
+        loss_function: _Loss | None = None,  # default l1 loss
+        **kwargs,
+    ) -> None:
         super().__init__()
         self.lr = lr
         self.weight_decay = weight_decay
+        self.loss_function = loss_function or L1Loss()
         self.model = HeteroGNN(**kwargs)
         self.save_hyperparameters()
+        self.val_loss_by_label: Dict[int, List[Tensor]] = defaultdict(list)
+
+    def loss(self, out, true_ys):
+        exp_label = (
+            true_ys.float()
+            if (
+                isinstance(self.loss_function, MSELoss)
+                and (true_ys.dtype == torch.int or true_ys.dtype == torch.long)
+            )
+            else true_ys
+        )
+        return self.loss_function(out, exp_label)
 
     def forward(self, x_dict, edge_index_dict, batch_dict):
         return self.model(x_dict, edge_index_dict, batch_dict)
 
     def training_step(self, data, batch_index) -> torch.Tensor:
-        return self._common_test_step(data, "train")
+        return self._common_step(data, "train")[1]
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
         return torch.optim.Adam(
             self.parameters(), lr=self.lr, weight_decay=self.weight_decay
         )
 
-    def _common_test_step(self, data, phase: str):
-        x_out = self.forward(data.x_dict, data.edge_index_dict, data.batch_dict)
-        loss = F.mse_loss(x_out, data.y.float())
-        self.log(f"{phase}_loss", loss, batch_size=data.batch_size)
-        return loss
+    def on_validation_epoch_start(self) -> None:
+        self.val_loss_by_label.clear()
+
+    def _common_step(self, batch, phase: str):
+        out = self.forward(batch.x_dict, batch.edge_index_dict, batch.batch_dict)
+        loss = self.loss(out, batch.y)
+        self.log(f"{phase}_loss", loss, batch_size=batch.batch_size)
+        return out, loss
 
     def validation_step(self, batch, batch_index):
-        return self._common_test_step(batch, "val")
+        out, val_loss = self._common_step(batch, "val")
+        for i, true_y in enumerate(batch.y):
+            true_y_key = true_y.item() if isinstance(true_y, torch.Tensor) else true_y
+            self.val_loss_by_label[true_y_key].append(self.loss(out[i], true_y).item())
+        return val_loss
+
+    def on_validation_epoch_end(self) -> None:
+        if wandb.run is None:
+            return
+        wandb.log(
+            {
+                f"val_loss/{label}": torch.tensor(losses).mean()
+                for label, losses in self.val_loss_by_label.items()
+            }
+        )
 
     def test_step(self, batch, batch_index):
-        return self._common_test_step(batch, "test")
+        return self._common_step(batch, "test")[1]
 
     def num_parameter(self) -> int:
         return sum(p.numel() for p in self.model.parameters() if p.requires_grad)
