5efaf6b9ec2117e0c599dba0558d1aaa37526630
jkrude <jakob.krude@hotmail.com>
2024-09-13 13:16:07 +0200

    Remove duplicate optimal_value_function

--- src/rgnet/rl/agents/__init__.py
+++ src/rgnet/rl/agents/__init__.py
@@ -1,5 +1,4 @@
 from .actor_critic import ActorCritic
 from .epsilon_greedy import EGreedyActorCriticHook, EGreedyModule, EpsilonAnnealing
 from .optimal_policy import OptimalPolicy, optimal_action
-from .optimal_value_function import OptimalValueFunction
 from .value_module import ValueModule
