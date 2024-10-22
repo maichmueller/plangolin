2d3c683bbadfda545f25179ca96785ff76a034a9
jkrude <jakob.krude@hotmail.com>
2024-10-22 14:27:04 +0200

    Added test instances and docstring to rl/data_layout.py

--- experiments/rl/data_layout.py
+++ experiments/rl/data_layout.py
@@ -3,9 +3,11 @@ import datetime
 import logging
 import warnings
 from pathlib import Path
-from typing import List, Optional, Sequence, Tuple
+from typing import List, Literal, Optional, Tuple
 
 import pymimir as mi
+
+from experiments import ROOT_DIR
 
 
 @dataclasses.dataclass
@@ -40,6 +42,10 @@ class OutputData:
         self.experiment_name = experiment_name
 
 
+def _all_or_filter(instances: List[str] | Literal["all"]):
+    return instances if isinstance(instances, list) else None
+
+
 class InputData:
     pddl_domains_dir: Path
     dataset_dir: Path
@@ -48,17 +54,52 @@ class InputData:
     _instances: List[Path]
     validation_problems: Optional[List[mi.Problem]]
     _validation_instances: Optional[List[Path]]
+    test_problems: Optional[List[mi.Problem]]
+    _test_instances: Optional[List[Path]]
 
     def __init__(
         self,
         domain_name: str,
         pddl_domains_dir: Path = Path("data/pddl_domains"),
         dataset_dir: Path = Path("data/flash_drives"),
-        instances: Sequence[str] | None = None,
-        validation_instances: List[str] | None = None,
+        train_subdir: str = "train",
+        eval_subdir: Optional[str] = None,
+        test_subdir: Optional[str] = None,
+        instances: List[str] | Literal["all"] = "all",
+        validation_instances: Optional[List[str]] | Literal["all"] = None,
+        test_instances: Optional[List[str]] | Literal["all"] = None,
         # if specified pddl_domains_dir and dataset_dir will be relative to root_dir
-        root_dir: Optional[Path] = None,
+        root_dir: Optional[Path] = ROOT_DIR,
     ):
+        """
+        Manages the data layout for input data to RL experiments.
+        The directories can either be specified as absolute paths or relative to the root_dir.
+        In basic layout inside pddl_domains_dir looks like
+        domain_name
+            domain.pddl
+            train
+                instance1.pddl
+        the domain is expected to be at pddl_domains_dir / domain_name /  "domain.pddl".
+        Evaluation and test problems can be either in a separate directories or in the train directory too.
+        Pass a list of specific instance names, or use "all" to include all available instances for each parameter.
+        Specifying validation or test instances is optional.
+
+        :param domain_name: The name of the domain, used to construct the full pddl_domains directory.
+        :param pddl_domains_dir: path to the pddl_resources (default: rgnet/data/pddl_domains)
+        :param dataset_dir: where to store datasets (default: rgnet/data/flash_drives)
+        :param train_subdir: the name of the subdirectory where the problem pddl files are located. (default: "train")
+        :param eval_subdir: the subdirectory where the validation problem files are located. (default: "train")
+        :param test_subdir: the subdirectory where the testing problem files are located. (default: "train")
+        :param instances: Either an explicit list of file names (or stems) or "all". (default: "all")
+        :param validation_instances: None to skip validation, an explicit list or "all". (default: None)
+        :param test_instances: None to skip testing, an explicit list of instances or "all". (default: None)
+        :param root_dir: Optional parent directory for both pddl_domains_dir and dataset_dir.
+            If specified the parameter pddl_domains_dir and dataset_dir will be interpreted as relative to root_dir.
+            (default: the project source directory)
+        """
+        self.train_subdir = train_subdir
+        self.eval_subdir = eval_subdir or self.train_subdir
+        self.test_subdir = test_subdir or self.train_subdir
         if root_dir:
             pddl_domains_dir = root_dir / pddl_domains_dir
             dataset_dir = root_dir / dataset_dir
@@ -76,16 +117,13 @@ class InputData:
             self.dataset_dir.mkdir(parents=True)
         self.pddl_domains_dir = pddl_domains_dir / domain_name
         if not self.pddl_domains_dir.exists() or not self.pddl_domains_dir.is_dir():
-            warnings.warn(
+            raise ValueError(
                 "Domain input directory does not exist or is not a directory.\n"
                 + str(self.pddl_domains_dir.absolute())
             )
         self._resolve_domain_and_instances(instances)
-        if validation_instances is not None:
-            self._resolve_validation_instances(validation_instances)
-        else:
-            self.validation_problems = None
-            self._validation_instances = None
+        self._resolve_validation_instances(validation_instances)
+        self._resolve_test_instances(test_instances)
 
         # We load state space lazily as it might be quite expensive
         self._spaces: Optional[List[mi.StateSpace]] = None
@@ -131,11 +169,25 @@ class InputData:
     def validation_problem_paths(self) -> Optional[List[Path]]:
         return self._validation_instances
 
+    @property
+    def test_problem_paths(self) -> Optional[List[Path]]:
+        return self._test_instances
+
     def _resolve_instances(
-        self, directory: Path, filter_list: List[str]
+        self, directory: Path, filter_list: List[str] | None
     ) -> Tuple[List[Path], List[mi.Problem]]:
+        """
+        Collects pddl problems from a directory, potentially filters them, and parses them as pymimir Problems.
+        :param directory: The directory in which the .pddl problems are in. Note the domain.pddl should not be in this directory.
+        :param filter_list: The list of instances to filter for, can be the full file name or only the stem. If None then all files will be used.
+        :return: The collected pddl files as paths and the respective parsed problems.
+        """
         assert directory.is_dir()
         all_instances = list(directory.glob("*.pddl"))
+        if any(instance_path.name == "domain.pddl" for instance_path in all_instances):
+            warnings.warn(
+                f"Found domain.pddl in {directory.resolve()} this will likely result in a parse exception. Only problems should be placed in this directory."
+            )
         if len(all_instances) == 0:
             warnings.warn(
                 "Could not find any *.pddl files in directory "
@@ -161,20 +213,44 @@ class InputData:
             for instance in all_instances
         ]
 
-    def _resolve_domain_and_instances(self, instances: List[str] | None = None):
+    def _resolve_domain_and_instances(self, instances: List[str] | Literal["all"]):
         self.domain: mi.Domain = mi.DomainParser(str(self.domain_path)).parse()
 
-        train_dir = self.pddl_domains_dir / "train"
-        instances, problems = self._resolve_instances(train_dir, instances)
-        self._instances = instances
+        train_dir = self.pddl_domains_dir / self.train_subdir
+        found_instances, problems = self._resolve_instances(
+            train_dir, _all_or_filter(instances)
+        )
+        self._instances = found_instances
         self.problems = problems
 
     def _resolve_validation_instances(
-        self, eval_instances: List[str], use_train_dir: bool = True
+        self,
+        validation_instances: Optional[List[str]] | Literal["all"],
     ):
+        if validation_instances is None:
+            self.validation_problems = None
+            self._validation_instances = None
+            return
+        else:
+            input_dir = self.pddl_domains_dir / self.eval_subdir
+            instances, problems = self._resolve_instances(
+                input_dir, _all_or_filter(validation_instances)
+            )
+            self.validation_problems = problems
+            self._validation_instances = instances
 
-        directory = "train" if use_train_dir else "eval"
-        input_dir = self.pddl_domains_dir / directory
-        instances, problems = self._resolve_instances(input_dir, eval_instances)
-        self.validation_problems = problems
-        self._validation_instances = instances
+    def _resolve_test_instances(
+        self,
+        test_instances: Optional[List[str]] | Literal["all"],
+    ):
+        if test_instances is None:
+            self.test_problems = None
+            self._test_instances = None
+            return
+        else:
+            input_dir = self.pddl_domains_dir / self.test_subdir
+            instances, problems = self._resolve_instances(
+                input_dir, _all_or_filter(test_instances)
+            )
+            self.test_problems = problems
+            self._test_instances = instances
