2e628a50be3ac79347bd3ad69c2d0e8aff14eeae
Michael Aichmueller <m.aichmueller@gmail.com>
2025-07-02 19:35:05 +0200

    partial atom drive can now use multiprocessing

--- src/rgnet/rl/data/atom_drive.py
+++ src/rgnet/rl/data/atom_drive.py
@@ -1,10 +1,13 @@
 import contextlib
 import itertools
-from collections import defaultdict, deque
+from collections import defaultdict
+from concurrent.futures import ProcessPoolExecutor
 from enum import Enum
 from itertools import chain
+from multiprocessing import RawValue
+from multiprocessing import synchronize as synchronize
 from pathlib import Path
-from typing import Iterable, List, Mapping, MutableMapping, Sequence
+from typing import Any, Iterable, List, Mapping, MutableMapping, Sequence
 
 import numpy as np
 import torch
@@ -20,7 +23,7 @@ from rgnet.rl.envs import (
     PlanningEnvironment,
     SuccessorEnvironment,
 )
-from rgnet.utils.misc import KeyAwareDefaultDict
+from rgnet.utils.misc import KeyAwareDefaultDict, env_aware_cpu_count, return_true
 from xmimir import (
     XAtom,
     XCategory,
@@ -52,12 +55,11 @@ def make_atom_ids(problem: XProblem) -> tuple[dict[str, int], list[XAtom]]:
     return atom_lookup, all_atoms
 
 
-def atom_tensor_to_dict(
+def atom_value_tensor_to_dict(
     atom_tensor: torch.Tensor,
-    atom_to_index_map: Mapping[str, int],
     index_to_atom_map: Mapping[int, str],
 ) -> list[dict[str, float]]:
-    atom_ids = torch.tensor(list(atom_to_index_map.values()), dtype=torch.int)
+    atom_ids = torch.tensor(list(index_to_atom_map.keys()), dtype=torch.int)
     assert (
         atom_ids == torch.arange(atom_ids.min().item(), atom_ids.max().item() + 1)
     ).all()
@@ -80,6 +82,10 @@ def atom_value_dict_to_tensor(
     assert (
         atom_ids == torch.arange(atom_ids.min().item(), atom_ids.max().item() + 1)
     ).all()
+    assert torch.unique(atom_ids).numel() == atom_ids.numel(), (
+        "Atom IDs must be unique, but found duplicates."
+        f" Unique IDs: {torch.unique(atom_ids)}, All IDs: {atom_ids}"
+    )
     atom_tensor = torch.full(
         (len(atom_values), len(atom_ids)), torch.inf, dtype=torch.float
     )
@@ -152,7 +158,7 @@ class AtomDrive(BaseDrive):
                     atom_values, self.atom_to_index_map
                 )
             case torch.Tensor():
-                atom_values_dict = atom_tensor_to_dict(
+                atom_values_dict = atom_value_tensor_to_dict(
                     atom_values, self.atom_to_index_map, self.index_to_atom_map
                 )
                 atom_values_tensor = atom_values
@@ -227,7 +233,7 @@ class AtomDrive(BaseDrive):
         return sorted(batched_data.values(), key=lambda d: d.idx)
 
     def atom_tensor_to_dict(self, atom_tensor: torch.Tensor) -> list[dict[str, float]]:
-        return atom_tensor_to_dict(
+        return atom_value_tensor_to_dict(
             atom_tensor, self.atom_to_index_map, self.index_to_atom_map
         )
 
@@ -375,12 +381,28 @@ class PartialAtomDrive(AtomDrive):
         iw_search: IWSearch,
         num_states: int = 0,
         seed: int = None,
+        max_processes: int | str = 1,
         **kwargs,
     ):
         self.iw_search = iw_search
         self.num_states = num_states
         self.seed = seed
         self._rng = None
+        match max_processes:
+            case "auto":
+                max_processes = env_aware_cpu_count()
+            case "all":
+                max_processes = env_aware_cpu_count()
+            case int():
+                max_processes = min(
+                    max_processes, env_aware_cpu_count(), self.num_states
+                )
+            case _:
+                raise ValueError(
+                    f"Invalid max_processes value: {max_processes}. "
+                    "Must be 'auto', 'all', or an integer."
+                )
+        self._max_processes = max_processes
         kwargs["env"] = None
         kwargs["atom_value_method"] = AtomValueMethod.IW
         super().__init__(*args, **kwargs)
@@ -401,6 +423,9 @@ class PartialAtomDrive(AtomDrive):
         return self._rng
 
     def get_space(self):
+        return None
+
+    def env_aux_data(self):
         return None
 
     @property
@@ -443,24 +468,17 @@ class PartialAtomDrive(AtomDrive):
             f"Building {self.__class__.__name__} "
             f"(problem: {problem.name} / {Path(problem.filepath).stem}, #space: {self.num_states} states (max-cutoff))"
         )
-        atom_values = self._sample_atom_dists(env)
+        atom_values: dict[XState, dict[str, float]] = self._sample_atom_dists(env)
         states = list(atom_values.keys())
         atom_id_map, atoms = make_atom_ids(problem)
+        counter = itertools.count(max(atom_id_map.values()) + 1)
+        for state, atom_value_dict in atom_values.items():
+            for atom, value in atom_value_dict.items():
+                if atom not in atom_id_map:
+                    atom_id_map[atom] = next(counter)
         self._atom_to_index_map = atom_id_map
-        match atom_values:
-            case dict():
-                atom_values_dict = list(atom_values.values())
-                atom_values_tensor = atom_value_dict_to_tensor(
-                    atom_values, self.atom_to_index_map
-                )
-            case torch.Tensor():
-                atom_values_dict = atom_tensor_to_dict(
-                    atom_values, self.atom_to_index_map, self.index_to_atom_map
-                )
-                atom_values_tensor = atom_values
-            case _:
-                raise ValueError(f"Unsupported atom values type: {type(atom_values)}")
-
+        atom_values_dict = list(atom_values.values())
+        atom_values_tensor = atom_value_dict_to_tensor(atom_values, atom_id_map)
         batched_data = self._encode(
             env, encoder, atom_values_dict, atom_values_tensor, states, self.num_states
         )
@@ -472,43 +490,264 @@ class PartialAtomDrive(AtomDrive):
 
     def _sample_atom_dists(
         self,
-        env: SuccessorEnvironment,
-    ) -> dict[XState, defaultdict[XAtom, float]] | torch.Tensor:
-        succ_gen = env.active_instances[0]
-        rng = np.random.default_rng(self.seed)
-        open_states = deque([succ_gen.initial_state])
-        dists: dict[XState, defaultdict[XAtom, float]] = dict()
+        env: PlanningEnvironment,
+    ) -> dict[XState, defaultdict[str, float]]:
+        from concurrent.futures import FIRST_COMPLETED, CancelledError, wait
+
+        succ_gen: XSuccessorGenerator = env.active_instances[0]
+        dists: dict[XState, defaultdict[str, float]] = dict()
         closed_states = set()
-        pbar: tqdm_asyncio | None
-        with (
+        # progress bar context
+        pbar_ctx = (
             tqdm(desc="Sampling Atom Distances", total=self.num_states)
             if self.show_progress
             else contextlib.nullcontext()
-        ) as pbar:
-            while len(closed_states) < self.num_states or not open_states:
-                state = open_states.pop()
-                closed_states.add(state)
-                atom_dists = defaultdict(lambda: float("-inf"))
-                dists[state] = atom_dists
-                collector = CollectorHook()
-                self.iw_search.solve(
-                    succ_gen,
-                    start_state=state,
-                    novel_hook=collector,
-                    stop_on_goal=False,
-                )
-                nodes = collector.nodes
-                for node in (nodes[i] for i in rng.permutation(len(nodes))):
-                    if node.state not in closed_states:
-                        open_states.append(node.state)
-                    for atom_tuples in node.novelty_trace[-1]:
-                        for atom in atom_tuples:
-                            if len(atom_tuples) == 1:
-                                # -1 because we always look at rewards!
-                                atom_dists[atom] = -1 * node.depth
-                if pbar:
-                    pbar.update(1)
-        return dists
+        )
+        if self._max_processes > 1:
+            pbar: tqdm_asyncio | None
+            stop_flag = RawValue("b", False)  # a single C‐char, no lock
+            init_args = (
+                stop_flag,
+                self.problem.domain.filepath,
+                self.problem.filepath,
+                self.iw_search,
+            )
+            with ProcessPoolExecutor(
+                max_workers=self._max_processes,
+                initargs=init_args,
+                initializer=init_worker,
+            ) as pool:
+                with pbar_ctx as pbar:
+                    # initial dispatch
+                    high_seed_end = 2**31 - 1
+                    futures = set()
+                    for _ in range(min(self._max_processes, self.num_states)):
+                        futures.add(
+                            pool.submit(
+                                _compute_state_dist_mp,
+                                int(self.rng.integers(low=0, high=high_seed_end)),
+                            )
+                        )
+                    completed_states = 0
+                    while completed_states < self.num_states:
+                        done_fs, undone_fs = wait(futures, return_when=FIRST_COMPLETED)
+                        futures = undone_fs
+                        while completed_states < self.num_states and done_fs:
+                            fut = done_fs.pop()
+                            if fut.cancelled():
+                                continue
+                            try:
+                                (
+                                    init_state_schemas,
+                                    init_state_object_bindings,
+                                    atom_dists,
+                                ) = fut.result()
+                            except CancelledError:
+                                continue
+                            # reconstruct state from serialized action trace
+                            state = rebuild_state(
+                                succ_gen, init_state_schemas, init_state_object_bindings
+                            )
+                            if state is None:
+                                state = succ_gen.initial_state
+                            if state not in closed_states:
+                                if pbar is not None:
+                                    pbar.update(1)
+                                closed_states.add(state)
+                                dists[state] = atom_dists
+                                completed_states += 1
+                                if completed_states == self.num_states:
+                                    stop_flag.value = True
+                                    break
+                            # sample the steps of steps we walk from the start to build the new starting state
+                            steps = int(self.rng.random() * len(init_state_schemas))
+                            # submit another task to compute the state distance
+                            futures.add(
+                                pool.submit(
+                                    _compute_state_dist_mp,
+                                    int(self.rng.integers(low=0, high=high_seed_end)),
+                                    True,  # serialize starting state
+                                    init_state_schemas[:steps],
+                                    init_state_object_bindings[:steps],
+                                )
+                            )
 
-    def env_aux_data(self):
-        return None
+            return dists
+        else:
+            # single process case
+            state = succ_gen.initial_state
+            with pbar_ctx as pbar:
+                for _ in range(self.num_states):
+                    _, atom_dists = compute_state_dist(
+                        state=state,
+                        succ_gen=succ_gen,
+                        action_schemas=None,
+                        object_bindings=None,
+                        iw_search=self.iw_search,
+                        serialize_starting_state=False,
+                        rng=self.rng,
+                    )
+                    dists[state] = atom_dists
+                    if state not in closed_states:
+                        closed_states.add(state)
+                    if pbar is not None:
+                        pbar.update(1)
+            return dists
+
+
+def init_worker(stop_flag, dom_path: str, prob_path: str, iw_search):
+    global _domain, _problem, _iw_search, _stop_flag
+    _domain, _problem = parse(dom_path, prob_path)
+    _iw_search = iw_search
+    _stop_flag = stop_flag
+
+
+def walk(
+    successor_gen: XSuccessorGenerator,
+    start_state=None,
+    rng: np.random.Generator | None = None,
+    bounds: tuple[int, int] = (1, 20),
+) -> tuple[XState, list[xmi.XAction]]:
+    """
+    Walks through the environment to collect some states.
+    """
+    if rng is None:
+        rng = globals().get("rng")
+        assert rng is not None, "No random number generator provided."
+    action_generator = successor_gen.action_generator
+    state = successor_gen.initial_state if start_state is None else start_state
+    nr_steps = rng.integers(*bounds)
+    chosen_actions = []
+    for _ in range(nr_steps):
+        actions = tuple(action_generator.generate_actions(state))
+        if not actions:
+            break
+        action = rng.choice(actions)
+        chosen_actions.append(action)
+        state = successor_gen.successor(state, action)
+    return state, chosen_actions
+
+
+def _compute_state_dist_mp(
+    seed: int | None = None,
+    serialize_starting_state: bool = True,
+    action_schemas: Sequence[int] | None = None,
+    object_bindings: Sequence[Sequence[str]] | None = None,
+):
+    global _iw_search, _problem, _stop_flag
+    succ_gen = XSuccessorGenerator(_problem)
+    return compute_state_dist(
+        state=rebuild_state(succ_gen, action_schemas, object_bindings),
+        succ_gen=succ_gen,
+        action_schemas=action_schemas,
+        object_bindings=object_bindings,
+        iw_search=_iw_search,
+        serialize_starting_state=serialize_starting_state,
+        rng=np.random.default_rng(seed),
+        stop_flag=_stop_flag,
+    )
+
+
+def compute_state_dist(
+    state: XState,
+    succ_gen: XSuccessorGenerator,
+    action_schemas: Sequence[int] | None = None,
+    object_bindings: Sequence[Sequence[str]] | None = None,
+    iw_search: IWSearch | None = None,
+    serialize_starting_state: bool = True,
+    rng: np.random.Generator | None = None,
+    stop_flag: Any | None = None,
+    bounds: tuple[int, int] = (0, 20),
+) -> (
+    tuple[Sequence[int], Sequence[str], dict[str, float]]
+    | tuple[XState, dict[str, float]]
+    | tuple[None, None]
+):
+    succ_gen = succ_gen
+    deadend = True
+    start_state = state
+    while deadend:
+        start_state, action_trace = walk(
+            succ_gen,
+            start_state=state,
+            rng=rng,
+            bounds=bounds,
+        )
+        if any(True for _ in succ_gen.action_generator.generate_actions(start_state)):
+            deadend = False
+            bounds = (bounds[0] + 2, bounds[1] + 2)
+    atom_dists = defaultdict(lambda: float("-inf"))
+    collector = CollectorHook()
+    if stop_flag is not None:
+        expansion_budget = lambda i: not stop_flag.value
+    else:
+        expansion_budget = return_true
+    iw_search.solve(
+        succ_gen,
+        start_state=start_state,
+        novel_hook=collector,
+        stop_on_goal=False,
+        expansion_budget=expansion_budget,
+    )
+    if stop_flag.value:
+        return None, None  # we will not need this output anymore
+    for node in collector.nodes:
+        for atom_tuples in node.novelty_trace[-1]:
+            if len(atom_tuples) == 1:
+                atom = atom_tuples[0]
+                reward = -1 * node.depth
+                atom_dists[str(atom)] = reward
+    if serialize_starting_state:
+        # serialized actions will allow to reconstruct the starting state from which IW was started
+        serialized_actions = list(
+            chain(
+                zip(action_schemas or [], object_bindings or []),
+                (_serialized_action(action) for action in action_trace),
+            )
+        )
+        schemas, bindings = zip(*serialized_actions) if serialized_actions else ([], [])
+        # atom dists will store the reward (negative distance) to each atom in the form of
+        #   (predicate_name, (object1_name, object2_name, ...)) --> reward
+        return schemas, bindings, dict(atom_dists)
+    else:
+        return start_state, dict(atom_dists)
+
+
+def _serialized_atom(atom: XAtom) -> tuple[str, tuple[str, ...]]:
+    return str(atom.predicate), tuple(o.get_name() for o in atom.objects)
+
+
+def _serialized_action(action: xmi.XAction) -> tuple[int, tuple[str, ...]]:
+    return action.action_schema.index, tuple(o.get_name() for o in action.objects)
+
+
+def rebuild_state(
+    succ_gen: XSuccessorGenerator,
+    action_schemas: Sequence[int],
+    object_bindings: Sequence[Sequence[str]],
+) -> XState | None:
+    if (
+        action_schemas is not None
+        and action_schemas
+        and object_bindings is not None
+        and object_bindings
+    ):
+        actual_schemas = [
+            succ_gen.problem.domain.actions[sid] for sid in action_schemas
+        ]
+        objects = succ_gen.problem.objects
+        object_names = list(map(lambda o: o.get_name(), objects))
+        bound_objects = [
+            [objects[object_names.index(o)] for o in binding]
+            for binding in object_bindings
+        ]
+        action_gen = succ_gen.action_generator
+        regrounded_actions = [
+            action_gen.ground_action(schema, objs)
+            for schema, objs in zip(actual_schemas, bound_objects)
+        ]
+        rebuilt_state = succ_gen.initial_state
+        for action in regrounded_actions:
+            rebuilt_state = succ_gen.successor(rebuilt_state, action)
+        return rebuilt_state
+    return None
